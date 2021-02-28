9272f6eb206b5dd091b316c0939bb01b5bdd2cf8
dzakyraffy <dzakyale-dzakly@yahoo.co.id>
2021-02-28 21:54:19 +0700

    still in preparation stages

--- modul1_sft_praktikumME.m
+++ modul1_sft_praktikumME.m
@@ -1,8 +1,14 @@
 %persiapan
+Fs=1;%sampling frequency
+L=200;%signal lenght(time observation)
+T=1/Fs;%sampling period
+P=2*sin(2*pi*0.2*t);%sinewave generator
+%%%butuh FFT algoritm
+t=(0:L-1)*T;
 m(t)=[];%sinyal informasi
 c(t)=[];%sinyal carrier
-theta1=(2*pi*f1)/sampling_freq;
-theta2=(2*pi*f2)/sampling_freq;
+theta1=(2*pi*f1)/Fs;
+theta2=(2*pi*f2)/Fs;
 N=1;%jumlah deret impuls
 %persiapan
 %USB Filter
