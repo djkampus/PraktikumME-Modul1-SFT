875a552a7c7afd366d72b346587bd15556cc4191
dzakyraffy <dzakyale-dzakly@yahoo.co.id>
2021-02-28 22:35:32 +0700

    workmarker2802
    ini sebenernya belum siap apapun
    jadi in masih ngetes udah bisa belum nampilin data atau apalah

--- modul1_sft_praktikumME.m
+++ modul1_sft_praktikumME.m
@@ -16,17 +16,18 @@ theta1=(2*pi*f1)/Fs;
 theta2=(2*pi*f2)/Fs;
 %persiapan
 %USB Filter
-usb_left=(theta2/pi)*sinc((theta2(n-0.5*N)/pi));
-usb_right=(theta1/pi)*sinc((theta1(n-0.5*N)/pi));
-h(n)=usb_left-usb_right;
+%usb_left=(theta2/pi)*sinc((theta2(n-0.5*N)/pi));
+%usb_right=(theta1/pi)*sinc((theta1(n-0.5*N)/pi));
+%h(n)=usb_left-usb_right;
 %USB Filter
 %Amplifier1
 %Amplifier1
 %Transmission line
+cable_lenght=20e3;
 attenuation=1/100;%atenuasi kabel yang dipakai(dB/m)
 cable_loss=attenuation*cable_lenght;
 gamma=1;%belum siap refrection coef
-gamma_db=10*log10*(1/1-(abs(gamma)^2));
+gamma_db=10*log10(1/1-(abs(gamma)^2));
 total_loss_line=10^((-1)*(cable_loss+gamma_db)/10);
 %Transmission line
 %Amplifier2
