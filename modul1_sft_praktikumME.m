935c24a64710d3477ebd65fc6de4a209e9d4e96f
dzakyraffy <dzakyale-dzakly@yahoo.co.id>
2021-02-28 23:58:12 +0700

    latest work for 2802
    yang masih bermasalah sebenernya banyak. mulai dari proses pembangkitan sinyal masih ngaco. belum ada filter yang siap. amplifier ngerjainnya gimana?
    terus  di transmisi kabel itu belum selesai di bagian reflection signal

--- modul1_sft_praktikumME.m
+++ modul1_sft_praktikumME.m
@@ -1,21 +1,26 @@
-%persiapan
-Fs=1;%sampling frequency
+%persiapan signal generation
+f_work=8e6;%frekuensi isyarat informasi
+%f_carrier=55e6;%frekuensi carrier
+power=15;%power kerja
+Fs=2*f_work;%sampling frequency
 T=1/Fs;%sampling period
-t=(0:L-1)*T;
-f1=10000;
-f2=11000;
+L=1000;%signal lenght(time observation)
+t=(0:L-1)*T;%time vector
 N=1000;%jumlah deret impuls
 n=(0:N-1);
-L=200;%signal lenght(time observation)
-P=2*sin(2*pi*0.2*t);%sinewave generator
+P=power*sin(2*pi*f_work*t);%sinewave generator
 P_fft=fft(P);%fourier transform(automatic module)
 P_fft_magnitude=abs(P_fft)./L;
-%m(t)=[];%sinyal informasi
-%c(t)=[];%sinyal carrier
-theta1=(2*pi*f1)/Fs;
-theta2=(2*pi*f2)/Fs;
+figure(1)
+plot(t,P)
 %persiapan
+%signal carrier
+%signal carrier
 %USB Filter
+%f1=10000;
+%f2=11000;
+%theta1=(2*pi*f1)/Fs;
+%theta2=(2*pi*f2)/Fs;
 %usb_left=(theta2/pi)*sinc((theta2(n-0.5*N)/pi));
 %usb_right=(theta1/pi)*sinc((theta1(n-0.5*N)/pi));
 %h(n)=usb_left-usb_right;
@@ -23,12 +28,14 @@ theta2=(2*pi*f2)/Fs;
 %Amplifier1
 %Amplifier1
 %Transmission line
-cable_lenght=20e3;
+cable_lenght=20e3;%spec b dengan 20km kabel
 attenuation=1/100;%atenuasi kabel yang dipakai(dB/m)
 cable_loss=attenuation*cable_lenght;
 gamma=1;%belum siap refrection coef
 gamma_db=10*log10(1/1-(abs(gamma)^2));
 total_loss_line=10^((-1)*(cable_loss+gamma_db)/10);
 %Transmission line
+%lowpass filter
+%lowpass filter
 %Amplifier2
 %Amplifier2
